40312e75b2155d3c399be18874d42e44f70526ed
James Dube <jamesddube@live.com>
2016-06-07 09:39:15 +0200

    added customer transformer

---
+++ app/Mps/Transformers/CustomerTransformer.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rick
+ * Date: 6/2/16
+ * Time: 10:32 PM
+ */
+
+namespace App\Mps\Transformers;
+
+
+class CustomerTransformer extends Transformer
+{
+    protected $transformKeys = [
+        'id',
+        'name',
+        'vat_number',
+        'address',
+        'telephone',
+        'fax',
+        'email',
+        'city',
+        
+
+    ];
+    
+    protected $key = 'customers';
+
+    public function transform($model)
+    {
+        return  [
+            'customer_id' => $model['id'],
+            'name' => $model['name'],
+            'vat_number' => $model['vat_number'],
+            'address' => $model['address'],
+            'telephone' => $model['telephone'],
+            'fax' => $model['fax'],
+            'email' => $model['email'],
+            'city' => $model['city'],
+
+        ];
+
+    }
+
+
+}
